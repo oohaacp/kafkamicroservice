48a03aed1eadabf82356fa3d815468943c310d22
oohaacp <oohaareddy@gmail.com>
2019-08-26 11:47:48 +0530

    KAFKADOCKERIZE IS FINISHED

---
+++ src/main/java/com/stackroute/kafkamicroservice/KafkamicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.kafkamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkamicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkamicroserviceApplication.class, args);
+	}
+
+}
